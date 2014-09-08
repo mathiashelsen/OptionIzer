2b8777c8e35a7b54c0d18f5b1a27a25b5d904858
Mathias Helsen <helsen.mathias@gmail.com>
2014-09-08 20:01:42 +0200

    added comments

--- myPDF.cpp
+++ myPDF.cpp
@@ -7,19 +7,20 @@ myPDF::myPDF(int nBins, double *x, int nValues)
     double maxValue = x[0], minValue = x[0];
     // Allocating some memory for the inverse CDF
     binValues = new double[nBins];
-
+    // Find minimal and maximal values
     for(int i = 0; i < nValues; i++)
     {
 	maxValue = (x[i] > maxValue) ? x[i] : maxValue;
 	minValue = (x[i] < minValue) ? x[i] : minValue;
     }
-
+    // from these deduce the spacing between bins
     binSpacing = (maxValue - minValue)/((double) nBins);
-
+    // so you can create the histogram
     for(int i = 0; i < nValues; i++ )
     {
 	tmpHisto[ (int) (x[i]/binSpacing) ]++;
     }
+    // from this the cumulative histogram
 
     for(int i = 0; i < nBins; i++)
     {
