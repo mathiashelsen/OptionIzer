9183a7945693006480a165cf275126e8cf573b73
Mathias Helsen <helsen.mathias@gmail.com>
2014-09-09 07:17:14 +0200

    added reference

--- myPDF.cpp
+++ myPDF.cpp
@@ -4,6 +4,7 @@ myPDF::myPDF(int nBins, double *x, int nValues)
 {
     // Local variables to be freed after initializing
     double * tmpHisto = new double[nBins];
+    double * tmpCDF = new double[nBins];
     double maxValue = x[0], minValue = x[0];
     // Allocating some memory for the inverse CDF
     binValues = new double[nBins];
@@ -20,13 +21,19 @@ myPDF::myPDF(int nBins, double *x, int nValues)
     {
 	tmpHisto[ (int) (x[i]/binSpacing) ]++;
     }
-    // from this the cumulative histogram
+    // from this the cumulative PDF
+    tmpCDF[0] = tmpHisto[0] / (double)nValues;
+    for(int i = 1; i < nBins; i++)
+    {
+	tmpCDF[i] = tmpCDF[i-1]/(double)nValues + tmpHisto[i];
+    }
 
     for(int i = 0; i < nBins; i++)
     {
 	std::cout << binSpacing * (double)i << "\t" << tmpHisto[i] << "\n";
     }
-
+   
+    delete[] tmpCDF; 
     delete[] tmpHisto;
 }
 
