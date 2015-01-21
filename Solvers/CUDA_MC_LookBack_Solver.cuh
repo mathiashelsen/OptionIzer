dc4c47a051a8f62d10defa4b62329a1bddc11cc1
Mathias Helsen <helsen.mathias@gmail.com>
2015-01-21 08:31:52 +0100

    implemented some more

--- Solvers/CUDA_MC_LookBack_Solver.cuh
+++ Solvers/CUDA_MC_LookBack_Solver.cuh
@@ -18,6 +18,28 @@ template<LookBackType type> __global__ void LookBackKernel(float *_x, float *_as
     int Nseries,
     int Nsteps)
 {
+    int pathIndex = threadIdx.x + blockIdx.x*blockDim.x;
+    if( pathIndex >= Nseries )
+	return;
+    
+    _assets[pathIndex] = S0;
+    float extr = S0;
+    for(int i = 0; i < Nsteps; i++)
+    {
+	_assets[pathIndex] *= (1.0 + r + sigma*_x[pathIndex*Nsteps + i]);
+	switch(type)
+	{
+	    case LookBackTypeMin:   extr = (_assets[pathIndex] < extr) ? _assets[pathIndex] : extr;
+				    break;
+	    case LookBackTypeMax:   extr = (_assets[pathIndex] > extr) ? _assets[pathIndex] : extr;
+				    break;
+	}
+    }
+
+    float tmp = call*(extr - K)*exp(-r*T);
+    _payoffs[pathIndex] = (tmp > 0.0f) ? tmp : 0.0f;
+
+    __syncthreads();
 
 }
 
